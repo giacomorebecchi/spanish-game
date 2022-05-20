dfddf4409aecf26df5af1ac284333526ff438359
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-05-20 10:29:57 +0200

    Renaming of excel source

--- src/spanish_game/definitions.py
+++ src/spanish_game/definitions.py
@@ -4,4 +4,4 @@ ROOT_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)
 )
 DATA_DIR = os.path.join(ROOT_DIR, "data")
-SPANISH_FILE = os.path.join(DATA_DIR, "Diccionario.xlsx")
+SPANISH_FILE = os.path.join(DATA_DIR, "vocabulary.xlsx")
