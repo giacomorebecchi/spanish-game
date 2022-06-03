c95fab3e40343ebb71202054735611186654720c
GIACOMO REBECCHI <grebecchi98@gmail.com>
2022-06-03 12:05:26 +0200

    New definitions for accents #2

--- src/spanish_game/definitions.py
+++ src/spanish_game/definitions.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 ROOT_DIR = os.path.realpath(
     os.path.join(os.path.dirname(__file__), os.path.pardir, os.path.pardir)
@@ -7,3 +8,20 @@ DATA_DIR = os.path.join(ROOT_DIR, "data")
 VOCABULARY_FILE = os.path.join(DATA_DIR, "vocabulary.xlsx")
 
 LANGUAGES = ["Italian", "Spanish"]
+
+_accent_equivalents = [
+    {"a", "á", "à"},
+    {"n", "ñ"},
+    {"e", "é", "è"},
+    {"i", "í", "ì"},
+    {"o", "ó", "ò"},
+    {"u", "ù", "ú"},
+]
+
+
+def _get_accent_equivalents(map_: List):
+    d = {char: {x for x in l if x != char} for l in map_ for char in l}
+    return d
+
+
+ACCENT_EQUIVALENTS = _get_accent_equivalents(_accent_equivalents)
